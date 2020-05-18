8e09c436f648de15961d3f1c873335e05c3c469e
DmitriyShvanyk <soledar1984@gmail.com>
2020-05-18 15:46:30 +0300

    add page main

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,11 @@
 import '../pages/index.css'
+
+
+
+// current years
+function setCurrentYears(elem){  
+  return elem.textContent = `${new Date().getFullYear()}`;
+}
+
+const footerYears = document.querySelector('.footer__years');
+setCurrentYears(footerYears);
